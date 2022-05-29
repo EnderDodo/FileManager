048134a670ccf55919c18fd64aa9d6b47aa23a93
Denis Urbansky <urbanskiy565@gmail.com>
2022-05-29 19:48:18 +0300

    init commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace FileManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.copy = new System.Windows.Forms.ToolStripMenuItem();
+            this.move = new System.Windows.Forms.ToolStripMenuItem();
+            this.delete = new System.Windows.Forms.ToolStripMenuItem();
+            this.archive = new System.Windows.Forms.ToolStripMenuItem();
+            this.search = new System.Windows.Forms.ToolStripMenuItem();
+            this.rename = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 31;
+            this.listBox1.Location = new System.Drawing.Point(12, 64);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(723, 562);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 31;
+            this.listBox2.Location = new System.Drawing.Point(755, 64);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(723, 562);
+            this.listBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 46);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Назад";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(147, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(585, 38);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(890, 17);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(585, 38);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(755, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 46);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Назад";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copy,
+            this.move,
+            this.delete,
+            this.archive,
+            this.search,
+            this.rename});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(211, 176);
+            // 
+            // copy
+            // 
+            this.copy.Name = "copy";
+            this.copy.Size = new System.Drawing.Size(210, 24);
+            this.copy.Text = "Копировать";
+            this.copy.Click += new System.EventHandler(this.copy_Click);
+            // 
+            // move
+            // 
+            this.move.Name = "move";
+            this.move.Size = new System.Drawing.Size(210, 24);
+            this.move.Text = "Переместить";
+            this.move.Click += new System.EventHandler(this.move_Click);
+            // 
+            // delete
+            // 
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(210, 24);
+            this.delete.Text = "Удалить";
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // archive
+            // 
+            this.archive.Name = "archive";
+            this.archive.Size = new System.Drawing.Size(210, 24);
+            this.archive.Text = "Архивировать";
+            this.archive.Click += new System.EventHandler(this.archive_Click);
+            // 
+            // search
+            // 
+            this.search.Name = "search";
+            this.search.Size = new System.Drawing.Size(210, 24);
+            this.search.Text = "Найти...";
+            // 
+            // rename
+            // 
+            this.rename.Name = "rename";
+            this.rename.Size = new System.Drawing.Size(210, 24);
+            this.rename.Text = "Переименовать";
+            this.rename.Click += new System.EventHandler(this.rename_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1489, 680);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem copy;
+        private System.Windows.Forms.ToolStripMenuItem move;
+        private System.Windows.Forms.ToolStripMenuItem delete;
+        private System.Windows.Forms.ToolStripMenuItem archive;
+        private System.Windows.Forms.ToolStripMenuItem search;
+        private System.Windows.Forms.ToolStripMenuItem rename;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,370 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.IO.Compression;
+
+namespace FileManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Load += Form1_Load;
+            listBox1.DoubleClick += ListBox1_DoubleClick;
+            listBox2.DoubleClick += ListBox2_DoubleClick;
+            listBox1.MouseUp += ListBox1_MouseUp;
+
+            textBox1.GotFocus += TextBox1_GotFocus;
+            textBox1.LostFocus += TextBox1_LostFocus;
+            textBox1.KeyDown += TextBox1_KeyDown;
+        }
+
+        string currPath1 = string.Empty;
+        string currPath2 = string.Empty;
+
+
+        private void OpenDirectory(ListBox listBox, string dirName, string currPath)
+        {
+            if (string.IsNullOrEmpty(dirName) && string.IsNullOrEmpty(currPath))
+                return;
+            if (dirName != null)
+            {
+                listBox.Items.Clear();
+                string path = Path.Combine(currPath, dirName);
+                DirectoryInfo currDir = new DirectoryInfo(path);
+                foreach (DirectoryInfo item in currDir.GetDirectories().Where(dir => !dir.Attributes.HasFlag(FileAttributes.Hidden | FileAttributes.System)))
+                {
+                    listBox.Items.Add('[' + item.ToString() + ']');
+                }
+                foreach (FileInfo item in currDir.GetFiles().Where(dir => !dir.Attributes.HasFlag(FileAttributes.Hidden | FileAttributes.System)))
+                {
+                    listBox.Items.Add(item);
+                }
+            }
+        }
+        private string Cleanse(string dir)
+        {
+            if (dir.StartsWith("["))
+            {
+                dir = dir.Remove(dir.Length - 1);
+                dir = dir.Remove(0, 1);
+            }
+            return dir;
+        }
+        private void ListBox1_DoubleClick(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+
+                string dir = Cleanse(listBox1.SelectedItem.ToString());
+                string path = Path.Combine(currPath1, dir);
+                if (Directory.Exists(path))
+                {
+                    OpenDirectory(listBox1, dir, currPath1);
+                    currPath1 = path;
+                    textBox1.Text = currPath1;
+                }
+                else if (File.Exists(path))
+                {
+                    Process.Start(path);
+                }
+            }
+        }
+        private void ListBox2_DoubleClick(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedItem != null)
+            {
+                string dir = Cleanse(listBox2.SelectedItem.ToString());
+                string path = Path.Combine(currPath2, dir);
+                if (Directory.Exists(path))
+                {
+                    OpenDirectory(listBox2, dir, currPath2);
+                    currPath2 = path;
+                    textBox2.Text = currPath2;
+                }
+                else if (File.Exists(path))
+                {
+                    Process.Start(path);
+                }
+            }
+        }
+        private void ListBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                if (listBox1.SelectedItem != null)
+                {
+                    contextMenuStrip1.Show(Cursor.Position);
+
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            foreach (DriveInfo item in allDrives)
+            {
+                listBox1.Items.Add(item.Name);
+                listBox2.Items.Add(item.Name);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(currPath1))
+                if (Directory.GetParent(currPath1) != null)
+                {
+                    currPath1 = Directory.GetParent(currPath1).FullName;
+                    OpenDirectory(listBox1, string.Empty, currPath1);
+                    textBox1.Text = currPath1;
+                }
+                else
+                {
+                    listBox1.Items.Clear();
+                    textBox1.Text = "";
+                    DriveInfo[] allDrives = DriveInfo.GetDrives();
+                    foreach (DriveInfo item in allDrives)
+                    {
+                        listBox1.Items.Add(item.Name);
+                    }
+                }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(currPath2))
+                if (Directory.GetParent(currPath2) != null)
+                {
+                    currPath2 = Directory.GetParent(currPath2).FullName;
+                    OpenDirectory(listBox2, string.Empty, currPath2);
+                    textBox2.Text = currPath2;
+                }
+                else
+                {
+                    listBox2.Items.Clear();
+                    textBox2.Text = "";
+                    DriveInfo[] allDrives = DriveInfo.GetDrives();
+                    foreach (DriveInfo item in allDrives)
+                    {
+                        listBox2.Items.Add(item.Name);
+                    }
+                }
+
+        }
+
+        private void copy_Click(object sender, EventArgs e)
+        {
+            Copy();
+        }
+        private void Copy()
+        {
+            string dir = Cleanse(listBox1.SelectedItem.ToString());
+            string path = Path.Combine(currPath1, dir);
+            try
+            {
+                if (!string.IsNullOrEmpty(currPath2))
+                {
+                    if (File.Exists(path))
+                    {
+                        File.Copy(path, currPath2);
+                    }
+
+                    if (Directory.Exists(path))
+                    {
+                        string distPath = Directory.CreateDirectory(Path.Combine(currPath2, dir)).FullName;
+
+                        CopyDirectory(path, distPath);
+                    }
+                }
+                OpenDirectory(listBox2, string.Empty, currPath2);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+        private void CopyDirectory(string sourceDir, string destinationDir)
+        {
+            var dir = new DirectoryInfo(sourceDir);
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            Directory.CreateDirectory(destinationDir);
+
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                string targetFilePath = Path.Combine(destinationDir, file.Name);
+                file.CopyTo(targetFilePath);
+            }
+
+            foreach (DirectoryInfo subDir in dirs)
+            {
+                string newDestinationDir = Path.Combine(destinationDir, subDir.Name);
+                CopyDirectory(subDir.FullName, newDestinationDir);
+            }
+        }
+        private void Delete()
+        {
+            string dir = Cleanse(listBox1.SelectedItem.ToString());
+            string path = Path.Combine(currPath1, dir);
+            try
+            {
+
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+
+                if (Directory.Exists(path))
+                {
+                    Directory.Delete(path, true);
+                }
+
+
+                OpenDirectory(listBox1, string.Empty, currPath1);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void move_Click(object sender, EventArgs e)
+        {
+            Copy();
+            Delete();
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+            Delete();
+        }
+
+        private void archive_Click(object sender, EventArgs e)
+        {
+            string dir = Cleanse(listBox1.SelectedItem.ToString());
+            string path = Path.Combine(currPath1, dir);
+            try
+            {
+                if (!string.IsNullOrEmpty(currPath2))
+                {
+                    if (File.Exists(path))
+                    {
+                        string zipName = Path.GetFileNameWithoutExtension(path) + ".zip";
+                        ArchiveFile(path, Path.Combine(currPath2, zipName));
+                    }
+
+                    if (Directory.Exists(path))
+                    {
+                        string zipName = Path.Combine(currPath2, dir + "ArchivedByZip.zip");
+
+                        File.Create(zipName).Close();
+                        ArchiveDirectory(path, zipName);
+                    }
+                }
+                OpenDirectory(listBox2, string.Empty, currPath2);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ArchiveFile(string filePath, string zipPath)
+        {
+            using (ZipArchive zip = ZipFile.Open(zipPath, ZipArchiveMode.Create))
+            {
+                zip.CreateEntryFromFile(filePath, Path.GetFileName(filePath));
+            }
+        }
+
+        private void ArchiveDirectory(string path, string zipPath)
+        {
+            using (ZipArchive zip = ZipFile.Open(zipPath, ZipArchiveMode.Update))
+            {
+                ZipArchiveEntry entry;
+                DirectoryInfo directory = new DirectoryInfo(path);
+                FileInfo[] files = directory.GetFiles("*");
+
+                foreach (FileInfo file in files)
+                {
+                    entry = zip.CreateEntryFromFile(Path.Combine(path, file.Name), directory.Name + "/" + file.Name);
+                }
+            }
+        }
+
+        private void rename_Click(object sender, EventArgs e)
+        {
+            isRenaming = true;
+            textBox1.Focus();
+        }
+
+        bool isRenaming;
+        private void TextBox1_GotFocus(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+        }
+
+        private void TextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (isRenaming)
+                {
+                    Rename();
+                }
+            }
+        }
+
+        private void Rename()
+        {
+            string dir = Cleanse(listBox1.SelectedItem.ToString());
+            string path = Path.Combine(currPath1, dir);
+            try
+            {
+                if (!string.IsNullOrEmpty(currPath1) && !string.IsNullOrEmpty(textBox1.Text))
+                {
+                    if (File.Exists(path))
+                    {
+                        FileInfo file = new FileInfo(path);
+                        string newPath = Path.Combine(Path.GetDirectoryName(path), textBox1.Text);
+
+                        file.MoveTo(newPath);
+                    }
+
+                    if (Directory.Exists(path))
+                    {
+                        DirectoryInfo directory = new DirectoryInfo(path);
+
+                        string newPath = Path.Combine(directory.Parent.FullName, textBox1.Text);
+                        Directory.Move(directory.FullName, newPath);
+                    }
+                }
+                OpenDirectory(listBox1, string.Empty, currPath1);
+                OpenDirectory(listBox2, string.Empty, currPath2);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            listBox1.Focus();
+        }
+
+        private void TextBox1_LostFocus(object sender, EventArgs e)
+        {
+            textBox1.Text = currPath1;
+        }
+    }
+}
